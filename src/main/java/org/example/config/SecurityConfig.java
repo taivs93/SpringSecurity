4bba1219f6ec53285dfb1202902f24c47919dc5c
taivs93 <taivs0903@gmail.com>
2025-06-06 18:13:06 +0700

    security update

--- src/main/java/org/example/config/SecurityConfig.java
+++ src/main/java/org/example/config/SecurityConfig.java
@@ -53,6 +53,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(request ->
                         request.requestMatchers("/user/**").permitAll()
                                 .requestMatchers("/admin/**").hasRole("ADMIN")
